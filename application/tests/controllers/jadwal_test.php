0372e0945ee4072d0e8ad509605a7f5df2d250f2
kveeen <widartakevin@gmail.com>
2017-10-18 13:48:44 +0700

    Add files via upload

--- application/tests/controllers/jadwal_test.php
+++ application/tests/controllers/jadwal_test.php
@@ -45,20 +45,60 @@ class Jadwal_test extends TestCase
 			);
                 $this->assertRedirect('dosen/jadwal', $output);
         }
-        
-        public function test_delete(){
-                $mula = $this->objek->getCurrentRow();
-		$id = 0;
+        public function test_form_update()
+        {
+        $_SESSION['nama'] = "ahmad1";
+        $_SESSION['group'] = "1";
+        $id = 0;
 		$result = $this->objek->getId('momon');
 		foreach ($result as $x){
 			$id = $x['jadwalid'];
 		}
-		$url = 'jadwal/delete/'.$id;
+        $url = 'dosen/jadwal/formupdate/'.$id;
+        $output = $this->request('GET', $url);
+        
+        //$contain = "<input class='form-control' id='cname' value='".$nama."' type='text' name='produk_nama' required />";
+        $this->assertContains('<title>dosen Page | Edit jadwal</title>' ,$output);
+        }
+        
+        public function test_update()
+        {
+            $result = $this->objek->getId('momon');
+    	foreach ($result as $x){
+			$id = $x['jadwalid'];
+		}
+    	$this->request(
+		'POST',
+		['dosen/jadwal', 'update/'.$id],
+		[ 
+		'nama' => 'momon',
+		'tanggal' => '19-10-2017',
+		'hari' => 'Kamis',
+		'jam' => '14:00:00',
+		]
+		);
+		$_SESSION['username'] = "ahmad1";
+                $_SESSION['group'] = "1";
+		$output = $this->request('GET', 'dosen/jadwal');;
+		$this->assertContains('<title>Sistem Monitoring Tugas Akhir | Jadwal</title>' ,$output);
+        }
+        
+        public function test_delete(){
+            $_SESSION['username'] = 'ahmad1';
+            $_SESSION['group'] = '1';    
+            $mula = $this->objek->getCurrentRow();
+            $id = 0;
+		$result = $this->objek->getId('momon');
+		foreach ($result as $x){
+			$id = $x['jadwalid'];
+		}
+		$url = 'dosen/jadwal/delete/'.$id;
 		$output = $this->request('GET', $url);
 		$akhir = $this->objek->getCurrentRow();
 	    $expected = $mula - $akhir;
+            
 	    $this->assertEquals(1, $expected);
-        }
+        }      
         
         public function test_cre_gagal()
         {
